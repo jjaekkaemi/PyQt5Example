c660bbb0818822f12eeacdb936e98c92f7e73bff
jaekyung <haroro@rainmaker.co.kr>
2025-01-10 16:14:40 +0900

    선택한 checkbox가 check 됐는지 체크

--- 02.04 CheckBox/checkboxTest.py
+++ 02.04 CheckBox/checkboxTest.py
@@ -10,29 +10,44 @@ class WindowClass(QMainWindow, form_class) :
         self.setupUi(self)
 
         #GroupBox밖에 있는 CheckBox에 기능 연결
-        self.chk_1.stateChanged.connect(self.chkFunction)
-        self.chk_2.stateChanged.connect(self.chkFunction)
-        self.chk_3.stateChanged.connect(self.chkFunction)
-        self.chk_4.stateChanged.connect(self.chkFunction)
+        self.chk_1.stateChanged.connect(lambda: self.chkFunction(1))
+        self.chk_2.stateChanged.connect(lambda: self.chkFunction(2))
+        self.chk_3.stateChanged.connect(lambda: self.chkFunction(3))
+        self.chk_4.stateChanged.connect(lambda: self.chkFunction(4))
 
         #GroupBox안에 있는 CheckBox에 기능 연결
-        self.groupchk_1.stateChanged.connect(self.groupchkFunction)
-        self.groupchk_2.stateChanged.connect(self.groupchkFunction)
-        self.groupchk_3.stateChanged.connect(self.groupchkFunction)
-        self.groupchk_4.stateChanged.connect(self.groupchkFunction)
+        self.groupchk_1.stateChanged.connect(lambda: self.groupchkFunction(1))
+        self.groupchk_2.stateChanged.connect(lambda: self.groupchkFunction(2))
+        self.groupchk_3.stateChanged.connect(lambda: self.groupchkFunction(3))
+        self.groupchk_4.stateChanged.connect(lambda: self.groupchkFunction(4))
 
-    def chkFunction(self) :
+    def chkFunction(self, num) :
         #CheckBox는 여러개가 선택될 수 있기 때문에 elif를 사용하지 않습니다.
-        if self.chk_1.isChecked() : print("chk_1 isChecked")
-        if self.chk_2.isChecked() : print("chk_2 isChecked")
-        if self.chk_3.isChecked() : print("chk_3 isChecked")
-        if self.chk_4.isChecked() : print("chk_4 isChecked")
+        if num == 1 :
+            if self.chk_1.isChecked() : print("chk_1 isChecked")
+            else : print("chk_1 isUnchecked")
+        elif num == 2 :
+            if self.chk_2.isChecked() : print("chk_2 isChecked")
+            else : print("chk_2 isUnchecked")
+        elif num == 3 :
+            if self.chk_3.isChecked() : print("chk_3 isChecked")
+            else : print("chk_3 isUnchecked")
+        elif num == 4 :
+            if self.chk_4.isChecked() : print("chk_4 isChecked")
+            else : print("chk_4 isUnchecked")
 
-    def groupchkFunction(self) :
-        if self.groupchk_1.isChecked() : print("groupchk_1 isChecked")
-        if self.groupchk_2.isChecked() : print("groupchk_2 isChecked")
-        if self.groupchk_3.isChecked() : print("groupchk_3 isChecked")
-        if self.groupchk_4.isChecked() : print("groupchk_4 isChecked")
+    def groupchkFunction(self, num) :
+        if num == 1 :
+            if self.groupchk_1.isChecked() : print("groupchk_1 isChecked")
+            else : print("groupchk_1 isUnchecked")
+        elif num == 2 :
+            if self.groupchk_2.isChecked() : print("groupchk_2 isChecked")
+            else : print("groupchk_2 isUnchecked")
+        elif num == 3 :
+            if self.groupchk_3.isChecked() : print("groupchk_3 isChecked")
+        elif num == 4 :
+            if self.groupchk_4.isChecked() : print("groupchk_4 isChecked")
+            else : print("groupchk_4 isUnchecked")
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
